c18f0f561c4e8419c3c2cc5fbe425a8d2aeb6897
tonis1000 <atonis@live.com>
2024-03-24 19:53:33 +0100

    Create proxy-script.js

---
+++ proxy-script.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const https = require('https');
+const url = require('url');
+
+http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url);
+  const options = {
+    hostname: parsedUrl.hostname,
+    port: parsedUrl.port || (parsedUrl.protocol === 'https:' ? 443 : 80),
+    path: parsedUrl.path,
+    method: req.method,
+    headers: req.headers
+  };
+
+  const proxyReq = (parsedUrl.protocol === 'https:' ? https : http).request(options, (proxyRes) => {
+    res.writeHead(proxyRes.statusCode, proxyRes.headers);
+    proxyRes.pipe(res, { end: true });
+  });
+
+  req.pipe(proxyReq, { end: true });
+
+  proxyReq.on('error', (err) => {
+    console.error('Proxy request error:', err);
+    res.statusCode = 500;
+    res.end('Proxy request failed');
+  });
+}).listen(8080, () => {
+  console.log('Proxy server running on port 8080');
+});
