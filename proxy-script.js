83cc98d78e3abd24f4a4df08b4316109380d679f
tonis1000 <atonis@live.com>
2024-04-01 20:12:53 +0200

    Update proxy-script.js

--- proxy-script.js
+++ proxy-script.js
@@ -1,14 +1,13 @@
 // Funktion zum Ausführen einer Proxy-Anfrage im Browser
 async function makeProxyRequest(url) {
   try {
-    // Hier wird die Anfrage an den externen Server gesendet
     const response = await fetch(url); // Fetch-API verwenden, um die Anfrage zu senden
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
     const responseData = await response.text(); // Response-Text abrufen
     console.log('Response data:', responseData); // Ausgabe des Response-Texts zur Überprüfung
-    return JSON.parse(responseData); // Die Daten als JSON zurückgeben
+    return responseData; // Die Daten zurückgeben
   } catch (error) {
     console.error('Proxy request failed:', error);
     throw error; // Fehler weiterleiten
