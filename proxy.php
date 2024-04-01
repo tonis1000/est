de12ff1303e8df1da38d35885a1676b81d1bef5a
tonis1000 <atonis@live.com>
2024-04-01 20:11:24 +0200

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -4,7 +4,12 @@ header("Content-Type: application/json");
 
 if (isset($_GET['url'])) {
     $url = urldecode($_GET['url']);
-    echo file_get_contents($url); // Die Inhalte der URL abrufen und zurückgeben
+    $contents = @file_get_contents($url); // Versuche, die Inhalte der URL abzurufen
+    if ($contents === FALSE) {
+        echo json_encode(['error' => 'Failed to fetch URL']);
+    } else {
+        echo $contents; // Die Inhalte der URL zurückgeben
+    }
 } else {
     echo json_encode(['error' => 'URL parameter is missing']);
 }
