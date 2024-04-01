7431bcb6eed010a714c49261c8dc46013dd5ca8b
tonis1000 <atonis@live.com>
2024-04-01 18:01:15 +0200

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -1,12 +1,11 @@
 <?php
 header("Access-Control-Allow-Origin: *"); // Erlaubt den Zugriff von allen Domains
-header("Content-Type: application/json"); // Setzt den Content-Type auf JSON
+header("Content-Type: application/json");
 
 if (isset($_GET['url'])) {
     $url = urldecode($_GET['url']);
-    $data = file_get_contents($url);
-    echo json_encode($data); // Konvertiert die Daten in JSON
+    echo file_get_contents($url); // Die Inhalte der URL abrufen und zurückgeben
 } else {
-    echo json_encode(array('error' => 'URL-Parameter fehlt')); // Fehlermeldung als JSON zurückgeben
+    echo json_encode(['error' => 'URL parameter is missing']);
 }
 ?>
