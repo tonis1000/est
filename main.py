8f0a15b18495664a9cf96866a326ed04a8b9e3cd
Your Name <your_email@example.com>
2024-04-05 20:58:24 +0200

    Vorhandene Projektdateien zu Git hinzufügen

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from datetime import datetime
 import pytz
 import codecs
 
-# Funktion zum Löschen des Inhalts der Ausgabedatei und Hinzufügen von Text
+# Funktion zum Hinzufügen von Text zur Ausgabedatei
 def append(text):
-    with codecs.open("epg-tmp.xml", "w", "utf-8") as f:
+    with codecs.open("epg-tmp.xml", "a", "utf-8") as f:
         f.write(text + '\n')
 
 # Funktion zum Generieren der Kanalübersicht und Programmzeiten
@@ -15,35 +15,35 @@ def generate_epg(xml_data):
     root = ET.fromstring(xml_data)
 
     # Iteriere durch jeden Kanal
-    for channel in root.findall('.//channel'):
-        channel_id = channel.find('display-name').text
-
-        # Überprüfen, ob das 'icon' vorhanden ist
-        icon_element = channel.find('.//icon')
-        if icon_element is not None:
-            channel_icon = icon_element.attrib.get('src')
-        else:
-            channel_icon = None
+    for channel in root.findall('channel'):
+        channel_id = channel.attrib.get('id')
+        channel_icon = None
+        for icon in channel.findall('icon'):
+            channel_icon = icon.attrib.get('src')
 
         # Informationen über den Kanal hinzufügen
-        append('<channel id="{}">'.format(channel_id))
-        append('<display-name>{}</display-name>'.format(escape(channel_id)))
-        if channel_icon:
-            append('<icon src="{}"/>'.format(escape(channel_icon)))
-        append('</channel>')
+        push(channel_id, channel_icon, channel.findall('programme'))
 
-        # Iteriere durch jedes Programm des Kanals
-        for program in channel.findall('.//programme'):
-            start = datetime.strptime(program.attrib['start'], "%Y%m%d%H%M%S %z").strftime('%Y%m%d%H%M%S %z')
+# Funktion zum Hinzufügen eines Kanals und seiner Programme zur Ausgabedatei
+def push(channel_id, icon, programs):
+    append('<channel id="{}">'.format(channel_id))
+    append('<display-name>{}</display-name>'.format(escape(channel_id)))
+    if icon:
+        append('<icon src="{}"/>'.format(escape(icon)))
+    append('</channel>')
 
-            stop = ''
-            if 'stop' in program.attrib:
-                stop = 'stop="{}"'.format(program.attrib['stop'])
+    # Iteriere durch jedes Programm des Kanals
+    for program in programs:
+        start = datetime.strptime(program.attrib['start'], "%Y%m%d%H%M%S %z").strftime('%Y%m%d%H%M%S %z')
 
-            append('<programme start="{}" {} channel="{}">'.format(start, stop, channel_id))
-            append('<title lang="el">{}</title>'.format(escape(program.find('title').text)))
-            append('<desc>{}</desc>'.format(escape(program.find('desc').text)))
-            append('</programme>')
+        stop = ''
+        if 'stop' in program.attrib:
+            stop = 'stop="{}"'.format(program.attrib['stop'])
+
+        append('<programme start="{}" {} channel="{}">'.format(start, stop, channel_id))
+        append('<title lang="el">{}</title>'.format(escape(program.find('title').text)))
+        append('<desc>{}</desc>'.format(escape(program.find('desc').text)))
+        append('</programme>')
 
 # Abrufen der XML-Daten von der externen URL
 url = "https://github.com/GreekTVApp/epg-greece-cyprus/releases/download/EPG/epg.xml"
